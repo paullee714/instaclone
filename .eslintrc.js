3c05282280ec30f24c9babc28d231a1abb30abcc
wool <paul_lee@kakao.com>
2021-09-10 21:59:35 +0900

    AppLoading Part

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: [
+        "react",
+    ],
+    rules: {
+        "global-require": 0,
+        "no-use-before-define": 0,
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        "react/style-prop-object": 0,
+        "react/jsx-indent-props": 0,
+        "require-jsdoc": 0,
+        "react/prop-types": 0,
+        indent: ["error", 4, { ignoredNodes: ["JSXElement"] }],
+        "react/jsx-indent": ["error", 4],
+        semi: [2, "always"],
+        quotes: [2, "double"],
+        "react/react-in-jsx-scope": "off",
+        "max-len": ["error", 150, 2, {
+            ignoreUrls: true,
+            ignoreComments: false,
+            ignoreRegExpLiterals: true,
+            ignoreStrings: false,
+            ignoreTemplateLiterals: false,
+        }],
+    },
+};
--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 
 ## 인스타그램앱 클론 프로젝트
 - React Native 사용
+
+### Lint
+```bash
+    npm install eslint
+    npx eslint --init
+```
+Lint를 사용해서 코드의 통일성을 지켜줍니다
+```
+    ✔ How would you like to use ESLint? · style
+    ✔ What type of modules does your project use? · esm
+    ✔ Which framework does your project use? · react
+    ✔ Does your project use TypeScript? · No / Yes
+    ✔ Where does your code run? · node✔ How would you like to define a style for your project? · guide
+    ✔ Which style guide do you want to follow? · airbnb
+    ✔ What format do you want your config file to be in? · JavaScript
+```
+
+### App Loading
+- [x] 엑스포를 사용한 앱 로딩 화면 만들기
+```bash
+    expo install expo-asset
+    expo install expo-app-loading
+    expo install expo-font
+    npm i @expo/vector-icons
+
+```
+아이콘 참고 링크 - [링크](https://icons.expo.fyi)
+유저가 앱을 실행하고 실제로 컴포넌트들이 나왔을 떄, 대부분의 데이터들이 준비되어 있어야 합니다. 
+미리 준비되는 작업들을 위해서 미리 로딩하는 시간을 갖는 앱 로딩 화면입니다.
+
+
