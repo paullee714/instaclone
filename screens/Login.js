328e7d560d2af2752e1508caa2bd6d54958c3e23
wool <paul_lee@kakao.com>
2021-09-12 00:24:02 +0900

    Moving Through Screens

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function Login() {
+export default function Login({ navigation }) {
     return (
         <View>
             <Text>Login</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+                <View>
+                    <Text>Go To Create Account</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
     return (
         <View>
             <Text>Welcome</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+                <View>
+                    <Text>Go To Create Account</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+                <View>
+                    <Text>Go To Login</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
