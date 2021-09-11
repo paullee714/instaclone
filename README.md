8e4f083c26aa62f94b24875905e60aa48344215a
wool <paul_lee@kakao.com>
2021-09-11 23:59:48 +0900

    Set Navigation

--- README.md
+++ README.md
@@ -32,4 +32,14 @@ Lint를 사용해서 코드의 통일성을 지켜줍니다
 유저가 앱을 실행하고 실제로 컴포넌트들이 나왔을 떄, 대부분의 데이터들이 준비되어 있어야 합니다. 
 미리 준비되는 작업들을 위해서 미리 로딩하는 시간을 갖는 앱 로딩 화면입니다.
 
-
+### Authentication
+- [x] 네비게이션을 사용해서 앱 로딩 후의 화면 Flow를 이어주기
+- [x] 유저 Create / Login 작업
+```bash
+    #react native navigation
+    npm install @react-navigation/native
+    yarn add @react-navigation/stack
+    expo install react-native-screens react-native-safe-area-context
+    expo install react-native-gesture-handler
+```
+- Navigation은 Stack/Tabs/Drawer가 있습니다
---
+++ navigators/LoggedOutNav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Welcome from "../screens/Welcome";
+import Login from "../screens/Login";
+import CreateAccount from "../screens/CreateAccount";
+
+const Stack = createStackNavigator();
+
+export default function LoggedOutNav() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="CreateAccount" component={CreateAccount} />
+        </Stack.Navigator>
+    );
+}
