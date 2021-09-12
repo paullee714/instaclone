5e8b263b7def05c82fd00528c7f21dda0688ed25
wool <paul_lee@kakao.com>
2021-09-12 17:15:59 +0900

    backend server setting with Ngrok & RN

--- apollo.js
+++ apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
+    uri: "https://615e-223-38-40-67.ngrok.io/graphql",
     cache: new InMemoryCache(),
 });
 
